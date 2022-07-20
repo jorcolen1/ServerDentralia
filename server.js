f1d3e3344b12a06f61409f947d86a664455f0cea
jorcolen1 <jorcolen@gmail.com>
2022-07-20 12:55:49 +0200

    quitar cors

--- server.js
+++ server.js
@@ -74,13 +74,13 @@ function ensureToken(req, res, next){
 //////0----------------------------------------------------------
 
 // Configurar cabeceras y cors
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
-}); 
+});  */
 
 app.use((req, res, next) => {
   const origin = req.headers.origin
